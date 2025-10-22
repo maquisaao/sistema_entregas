c1646ee7afd6bfde0aa95d72ae72008a6c775793
maquisaao <maxwpinheiro@gmail.com>
2025-10-22 14:10:21 -0300

    configuracao do banco de dados (postgreSQL)

---
+++ backend/sistema_entregas/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sistema_entregas_db',
+        'USER': 'postgres',
+        'PASSWORD': 'sua_senha_aqui',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
